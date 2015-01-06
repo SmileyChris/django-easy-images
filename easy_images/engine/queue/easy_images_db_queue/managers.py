0658dbe076fec7469eb9a10ae6848963f0739aa5
Chris Beaven <smileychris@gmail.com>
2015-01-06 17:07:16 +1300

    Manager is a manager, not a model :P

--- easy_images/engine/queue/easy_images_db_queue/managers.py
+++ easy_images/engine/queue/easy_images_db_queue/managers.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-class ActionManager(models.Model):
+class ActionManager(models.Manager):
 
     def queue(self, limit=120):
         """
