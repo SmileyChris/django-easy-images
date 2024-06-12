14a2150395920cf98df4f7178937d6940cfd6b7b
Chris Beaven <smileychris@gmail.com>
2024-06-12 17:34:44 +1200

    Add tests for basic output from an actual model's filefield

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,13 +1,19 @@
 from io import BytesIO
 
 import pytest
+from django.core.files.uploadedfile import SimpleUploadedFile
 
+from easy_images.core import Img
 from easy_images.models import (
     EasyImage,
     ImageStatus,
     get_storage_name,
     pick_image_storage,
 )
+from pyvips.vimage import Image
+from tests.easy_images_tests.models import Profile
+
+thumbnail = Img(width=200)
 
 
 @pytest.mark.django_db
@@ -36,3 +42,25 @@ def test_build_no_source():
     image.build()
     assert image.status == ImageStatus.SOURCE_ERROR
     assert image.error_count == 1
+
+
+@pytest.mark.django_db
+def test_build_from_filefield():
+    image = Image.black(1000, 1000)
+    file = SimpleUploadedFile("test.png", image.write_to_buffer(".png[Q=90]"))
+    profile = Profile.objects.create(name="Test", image=file)
+
+    thumb = thumbnail(profile.image)
+    assert thumb.base_url() == "/profile-images/test.png"
+    assert thumb.as_html() == '<img src="/profile-images/test.png" alt="">'
+
+
+@pytest.mark.django_db
+def test_build_from_filefield_with_build():
+    image = Image.black(1000, 1000)
+    file = SimpleUploadedFile("test.png", image.write_to_buffer(".png[Q=90]"))
+    profile = Profile.objects.create(name="Test", image=file)
+
+    thumb = thumbnail(profile.image, build="src")
+    assert thumb.base_url().endswith(".jpg")
+    assert thumb.as_html() == f'<img src="{thumb.base_url()}" alt="">'
